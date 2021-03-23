b98a38de642e3090196ae9b895be07ba392ebcb4
Gonzalo Varela <gonzalo.varela1981@gmail.com>
2021-03-22 21:06:44 -0300

    Se completa la iteracion de la matriz en todas las direcciones y validar si la secuencia coincide con dna mutante, resta iterar sobre cada linea

--- README.md
+++ README.md
@@ -18,6 +18,6 @@ Abrir el navegador e ingresar a la direccion 0.0.0.0:8090
 
 #### To do
 - Mejorar Dockerfile utilizando mutli-stage builds https://www.docker.com/blog/containerized-python-development-part-1/
-- Recorrer la Matriz evaluando cada secuencia en 3 direcciones
+- Validar todas las secuencias que caben en una linea, tener en cuenta que cuando una secuencia coincide con una secuencia mutante no puede reutilizarse. Ej AAAAAG no puede devolver 2 coincidencias
 - Crear API REST que devuelve http 200-OK si un humano es mutante y HTTP 403-Forbidden si no lo es. La entrada es un JSON y el endpoint es /mutant
 - Persistir los resultados en una base de datos y exponer en el endpoint /stats el resultado en formato JSON con el siguiente formato {"count_mutant_dna":40, "count_human_dna":100, "ratio":0.4}
--- app/main.py
+++ app/main.py
@@ -15,17 +15,20 @@ def main():
 	'''
 	Obtengo una serie de secuencias, valido el formato y verifico si pertenece a un mutante
 	'''
-	dna = '{"ATGCGA","CAGTGC","TTATGT","AGAtGG","CCCCTA","TCACTG"};'
+	dna = '{"ATGCGA","CAGGGG","TTATGT","AGAAGA","ACCCTA","CCCCTA"};'
     
     # Genero la matriz desde el string recibido
 	matriz = validarMatriz(dna)
-
-	if(isMutant('aaaa', 'b')):
+	
+	# Iteracion de la Matriz
+	count_match = iterarMatriz(matriz)
+	#if(isMutant('aaaa', 'a')):
+	if(count_match >= _CONFIG["NUM_SEQUENCE_MATCH"]):
 		return ''
 	else:
 		abort(_ERROR_CODES["NOT_MUTANT"], '') # Se envía response code 403 sin mensaje, flask agrega Forbidden por default
 
-def isMutant(adn, sample):
+def isMutant(sequence, base):
 	'''
 	Devuelve True si la secuencia coincide con una secuencia mutante o False si no lo hace
 	Si la secuencia repite una base SEQUENCE_LENGTH veces se considera secuencia mutante
@@ -36,14 +39,16 @@ def isMutant(adn, sample):
 	La funcion que recorre la matriz es independiente de la funcion que evalua
 	la secuencia de ADN
 	'''
-	sequence = []
+	match_str = []
 	# Con la primer base de la secuencia genero un string 
 	for i in range(_CONFIG['SEQUENCE_LENGTH']):
-		sequence.append(str(sample))
+		match_str.append(str(base))
 
-	match_str = str('').join(sequence)
+	match_sequence= str('').join(match_str)
+	match_seq_upper = match_sequence.upper()
+	seq_upper = sequence[0:_CONFIG['SEQUENCE_LENGTH']].upper()
 
-	if (adn.upper() == match_str.upper()):
+	if (seq_upper == match_seq_upper):
 		return True
 	else :
 		return False
@@ -92,6 +97,84 @@ def validarTamañoSequence(sequence, size):
 		raise InvalidUsage('La matriz recibida no es valida, una secuencia es mas corta;', status_code=_ERROR_CODES["SEQUENCE_LENGTH_MISMATCH"])
 	if(iSize < _CONFIG["SEQUENCE_LENGTH"]):
 		raise InvalidUsage('La matriz recibida no es valida, la secuencia es muy corta;', status_code=_ERROR_CODES["SEQUENCE_TO_SHORT"])
+
+def iterarMatriz(matriz):
+	# Calculo la cantidad de filas y columnas de la matriz
+	size_matriz_x = len(matriz)
+	size_matriz_y = len(matriz[0])
+	max_x = len(matriz[0]) - _CONFIG["SEQUENCE_LENGTH"]
+	max_y = len(matriz) - _CONFIG["SEQUENCE_LENGTH"]
+	count_match = 0 # Cuento cuantas secuencias corresponden a ADN mutante
+	for loop in range(0, size_matriz_x ):
+		print('Vuelta ', loop, 'match_count', count_match)
+		# Mientras el numero de coincidencias sea menor al requerido seguir buscando
+		if(count_match < _CONFIG["NUM_SEQUENCE_MATCH"]):
+			# tomo toda la fila vertical para iterar
+			string_vertical = ''
+			for count in  range(0, size_matriz_y):
+				string_vertical += matriz[count][loop]
+			# Se imprime la linea para verificacion en la etapa de desarrollo
+			print('\t Vertical ', string_vertical)
+			if(isMutant(string_vertical, string_vertical[0])):
+				count_match += 1
+
+			# tomo toda la fila horizontal para iterar
+			string_horizontal = str(matriz[loop])
+			# Se imprime la linea para verificacion en la etapa de desarrollo
+			print('\t Horizontal ', string_horizontal)
+			if(isMutant(string_horizontal, string_horizontal[0])):
+				count_match += 1
+			
+			# tomo las secuencias Diagonales  para iterar
+			positions_y = list(range(0, size_matriz_y - loop))
+
+			string_diagonal_a = ''
+			for count in  positions_y:
+				string_diagonal_a += matriz[count + loop][count]
+			# Se imprime la linea para verificacion en la etapa de desarrollo
+			print('\t Diagonal A ', string_diagonal_a)
+			if(isMutant(string_diagonal_a, string_diagonal_a[0])):
+				count_match += 1
+
+			# En la primer vuelta diagonal_a y diagonal_b es la misma secuencia
+			# y por eso se ignora
+			if (loop != 0):
+				string_diagonal_b = ''
+				for count in  positions_y:
+					string_diagonal_b += matriz[count][count + loop]
+				# Se imprime la linea para verificacion en la etapa de desarrollo
+				print('\t Diagonal B ', string_diagonal_b)
+				if(isMutant(string_diagonal_b, string_diagonal_b[0])):
+					count_match += 1
+
+			# tomo las secuencias Diagonales en direccion inversa para iterar
+			positions_y_reverse = positions_y.copy()
+			positions_y_reverse.sort(reverse=True)
+
+			# En la primer vuelta diagonal_c y diagonal_d es la misma secuencia
+			# y por eso se ignora
+			if (loop != 0):
+				string_diagonal_c = ''
+				for count in  positions_y:
+					string_diagonal_c += matriz[positions_y_reverse[count]][count]
+				# Se imprime la linea para verificacion en la etapa de desarrollo
+				print('\t Diagonal C ', string_diagonal_c)
+				if(isMutant(string_diagonal_c, string_diagonal_c[0])):
+					count_match += 1
+
+			string_diagonal_d = ''
+			for count in  positions_y:	
+				string_diagonal_d += matriz[positions_y_reverse[count] + loop][count + loop]
+			# Se imprime la linea para verificacion en la etapa de desarrollo
+			print('\t Diagonal D ', string_diagonal_d)
+			if(isMutant(string_diagonal_d, string_diagonal_d[0])):
+				count_match += 1
+		else:
+			# Hay count_match coincidencias, detener Loop
+			break
+
+	print(matriz)
+	return count_match
 
 # Constants
 _ERROR_CODES = {"NOT_MUTANT":403,"SEQUENCE_LENGTH_MISMATCH":514,"SEQUENCE_TO_SHORT":513,"INVALID_BASE":512,"IVALID_STRING_IDENTIFIER":511,"INVALID_BSTRING_HEADERS":510}
